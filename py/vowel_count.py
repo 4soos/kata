39986bf697bf04730cb355cb88399fc40f5befb1
Young <young_2017@126.com>
2020-12-01 01:26:47 +0800

    [Added][vowel count]

---
+++ py/vowel_count.py
@@ -0,0 +1,10 @@
+def get_count(input_str):
+    num_vowels = 0
+    # your code here
+    num_vowels += input_str.count('a')
+    num_vowels += input_str.count('e')
+    num_vowels += input_str.count('i')
+    num_vowels += input_str.count('o')
+    num_vowels += input_str.count('u')
+
+    return num_vowels
